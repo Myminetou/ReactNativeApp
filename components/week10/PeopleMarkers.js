8d85ad5c40d0a9990c380ccc4e17a3f1a9769c63
Myminetou <jasmine100272@gmail.com>
2024-04-03 14:40:16 +0700

    week09&week10

---
+++ components/week10/PeopleMarkers.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function PeopleMarkers(props) {
+
+  return props.items.map((item) => (
+    <Marker
+      coordinate={{
+        "latitude": Number(item.latitude),
+        "longitude": Number(item.longitude)
+      }}
+      title={item.name}
+      key={item.id.toString()}
+      description={item.address}
+    >
+        <FontAwesome name="user" size={20} color="tomato" />
+    </Marker>
+  ));
+}
+
+
